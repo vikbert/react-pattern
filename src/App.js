f747f3bb055854f0f3a27de6fe09b95ef1cfa9e3
Xun Zhou <xun.zhou@mail.schwarz>
2019-08-14 22:55:26 +0200

    init commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Hello World
+      </header>
+    </div>
+  );
+}
